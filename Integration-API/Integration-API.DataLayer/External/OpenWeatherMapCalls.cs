1498afbd054c78a5ce1387531810ec110050e0ed
TeunMos <teun.mos@gmail.com>
2022-11-14 15:32:22 +0100

    create project structure

---
+++ Integration-API/Integration-API.DataLayer/External/OpenWeatherMapCalls.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Integration_API.Models.OpenWeatherMap;
+
+namespace Integration_API.DataLayer.External
+{
+    public class OpenWeatherMapCalls
+    {
+        private readonly string ApiKey;
+        private readonly string BaseUrl = "https://api.openweathermap.org/data/2.5/";
+        public OpenWeatherMapCalls(string apiKey)
+        {
+            ApiKey = apiKey;
+        }
+
+        public async Task<string> GetCurrentLocalWeather(OpenWeatherMapCredentials credentials)
+        {
+            string city = credentials.cityName;
+            Regex rgx = new Regex("[?&=]");
+            city = rgx.Replace(city, "");
+
+            string urlParams = "weather?q= "+ city + "&appid=" + ApiKey;
+            string url = BaseUrl + urlParams;
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response =  await client.GetStringAsync(url);
+                return response;
+            }
+
+        }
+
+    }
+}
---
+++ Integration-API/Integration-API.Models/OpenWeatherMap/OpenWeatherMapResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integration_API.Models.OpenWeatherMap
+{
+    public class OpenWeatherMapResponse
+    {
+        public double tempInCelcius;
+        public string imgName;
+    }
+}
