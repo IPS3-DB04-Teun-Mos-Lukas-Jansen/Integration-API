0698cabbf11a38d21f045bf22edfee7e986370ea
TeunMos <teun.mos@gmail.com>
2022-11-22 09:40:38 +0100

    reverted comments

--- Integration-API/TestConsole/Program.cs
+++ Integration-API/TestConsole/Program.cs
@@ -4,4 +4,16 @@ using Integration_API.DataLayer;
 using Integration_API.DataLayer.Internal;
 using Integration_API.LogicLayer;
 using MongoDB.Bson;
+
+//OpenWeatherMapService service = new OpenWeatherMapService(calls, dataAcces);
+
+
+//OpenWeatherMapResponse response = await service.GetLocalWeatherForecast("1");
+
+
+//Console.WriteLine(response.tempInCelcius);
+
+
+//Console.WriteLine(Directory.GetCurrentDirectory());
+
 Console.ReadLine();
