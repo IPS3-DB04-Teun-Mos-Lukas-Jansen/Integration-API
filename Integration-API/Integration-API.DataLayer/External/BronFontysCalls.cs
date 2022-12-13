6e43192123d25be204545bef95917de277c4dd3c
TeunMos <teun.mos@gmail.com>
2022-12-13 11:11:03 +0100

    added an integration for https://bron.fontys.nl/

---
+++ Integration-API/Integration-API.DataLayer/External/BronFontysCalls.cs
@@ -0,0 +1,33 @@
+﻿using Integration_API.DataLayer.External;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Integration_API.DataLayer.External
+{
+    public class BronFontysCalls : IBronFontysCalls
+    {
+        private const string _bronFontysUrl = "https://bron.fontys.nl/feed/nl";
+
+        public async Task<SyndicationFeed> GetNewsFeed()
+        {
+            //using (HttpClient client = new HttpClient())
+            //{
+            //    var response = await client.GetStringAsync(_bronFontysUrl);
+            //    return response;
+            //}
+            //try { }
+            using (var reader = XmlReader.Create(_bronFontysUrl))
+            {
+                SyndicationFeed response = SyndicationFeed.Load(reader);
+                return response;
+            }
+            //catch { } // TODO: Deal with unavailable resource.
+
+        }
+    }
+}
---
+++ Integration-API/Integration-API.LogicLayer/Interfaces/IBronFontysService.cs
@@ -0,0 +1,10 @@
+﻿using Integration_API.Models.BronFontys;
+
+namespace Integration_API.LogicLayer
+{
+    public interface IBronFontysService
+    {
+        Task<List<BronFontysResponse>> GetFeed(string UserId);
+        Task SetBronFontysCredentials(string UserId, BronFontysCredentials credentials);
+    }
+}
